e7f31034f207db6d684b2d1671784b1ec1c20970
JEFERSON DIONEI SEHNEM <sehnemjeferson@gmail.com>
2025-01-24 10:35:25 -0300

    Adcionando ao script python delete dos dados apos o concat

--- codigos_rodando/juncao_balanco_patrimonial.py
+++ codigos_rodando/juncao_balanco_patrimonial.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import os
 
 df1 = pd.read_csv('../dados/resumo_balanco_ate_100.csv', index_col=0)
 
@@ -15,4 +16,14 @@ print('Juntou df1 e df2')
 df_junto.to_csv('../dados/resumo_balanco.csv')
 
 print('Salvou df_junto')
+
+os.remove('../dados/resumo_balanco_ate_100.csv')
+
+print('Removeu resumo_balanco_ate_100.csv')
+
+os.remove('../dados/resumo_balanco_acima_100.csv')
+
+print('Removeu resumo_balanco_acima_100.csv')
+
+print('Fim do script')
 
