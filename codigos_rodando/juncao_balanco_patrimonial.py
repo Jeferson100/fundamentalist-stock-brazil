8b906e1f82089c24dbe00232056500bbdee01a09
JEFERSON DIONEI SEHNEM <sehnemjeferson@gmail.com>
2025-01-24 10:28:11 -0300

    Criando script para juncao dos dois balancos

---
+++ codigos_rodando/juncao_balanco_patrimonial.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+df1 = pd.read_csv('../dados/resumo_balanco_ate_100.csv', index_col=0)
+
+print('Carregou df1')
+
+df2 = pd.read_csv('../dados/resumo_balanco_acima_100.csv', index_col=0)
+
+print('Carregou df2')
+
+df_junto = pd.concat([df1, df2])
+
+print('Juntou df1 e df2')
+
+df_junto.to_csv('../dados/resumo_balanco.csv')
+
+print('Salvou df_junto')
+
